3a4e728ba7d0f0b575fd845213978f5c766225cb
alibabaumbala <vanhuy1619@gmail.com>
2023-07-03 15:03:20 +0700

    zipkin

--- starter1/src/main/java/com/example/starter/Main.java
+++ starter1/src/main/java/com/example/starter/Main.java
@@ -1,11 +1,44 @@
 package com.example.starter;
 
+import com.example.starter.api.constant.PropertiesConfig;
+import com.example.starter.utils.ConfigUtils;
+import com.example.starter.utils.StackTraceUtil;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.micrometer.MicrometerMetricsOptions;
+import io.vertx.micrometer.VertxPrometheusOptions;
+import io.vertx.tracing.zipkin.HttpSenderOptions;
+import io.vertx.tracing.zipkin.ZipkinTracingOptions;
 
 public class Main {
   public static void main(String []args)
   {
-    Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new MainVerticle());
+    PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    new UptimeMetrics().bindTo(registry);
+
+    Vertx vertx = Vertx.vertx(new VertxOptions()
+      .setWorkerPoolSize(500)
+      .setMetricsOptions(
+        new MicrometerMetricsOptions()
+          .setPrometheusOptions(new VertxPrometheusOptions().setEnabled(true))
+          .setJvmMetricsEnabled(true)
+          .setMicrometerRegistry(registry)
+          .setEnabled(true)
+      )
+      .setTracingOptions(new ZipkinTracingOptions()
+        .setServiceName(ConfigUtils.getInstance().getProperties().getProperty(PropertiesConfig.SERVICE_NAME))
+        .setSenderOptions(new HttpSenderOptions()
+          .setSenderEndpoint(ConfigUtils.getInstance().getProperties().getProperty(PropertiesConfig.ZIPKIN_ENDPOINT))
+          .setSsl(true))
+      ));
+
+    vertx.deployVerticle(MainVerticle.class.getName())
+      .onFailure(throwable -> {
+        System.out.println(StackTraceUtil.getStackTrace(throwable));
+        System.exit(-1);
+      });
   }
 }
--- starter1/src/main/java/com/example/starter/api/constant/PropertiesConfig.java
+++ starter1/src/main/java/com/example/starter/api/constant/PropertiesConfig.java
@@ -2,6 +2,11 @@ package com.example.starter.api.constant;
 
 public interface PropertiesConfig {
 
+  public final static String SERVICE_NAME = "service.name";
+
   public final static String SERVER_PORT = "server.port";
   public final static String TEMPORAL_QUEUE_NAME = "";
+
+  public final static String ZIPKIN_ENDPOINT = "zipkin.endpoint";
+
 }
